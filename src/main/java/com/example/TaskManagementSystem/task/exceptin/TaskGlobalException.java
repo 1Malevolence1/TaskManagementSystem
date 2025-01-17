291995c18a2074ee7661edf31bb360a122302163
Mr.D <86906896+1Malevolence1@users.noreply.github.com>
2025-01-17 12:45:31 +0300

    добавил обработчик ошибок для Task

--- src/main/java/com/example/TaskManagementSystem/task/exceptin/TaskGlobalException.java
+++ src/main/java/com/example/TaskManagementSystem/task/exceptin/TaskGlobalException.java
@@ -1,4 +1,16 @@
 package com.example.TaskManagementSystem.task.exceptin;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
 public class TaskGlobalException {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Validate> handlerNoSuchElementException(NoSuchElementException e){
+        return ResponseEntity.badRequest().body(new Validate(e.getMessage()));
+    }
 }
---
+++ src/main/java/com/example/TaskManagementSystem/task/exceptin/Validate.java
@@ -0,0 +1,12 @@
+package com.example.TaskManagementSystem.task.exceptin;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Validate {
+    String error;
+
+}
